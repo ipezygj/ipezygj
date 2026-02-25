a1b16aec604226e01e44096564b018f3e6a3fa3c
ipezygj <ipezygj2@gmail.com>
2026-02-26 01:09:49 +0200

    feat: add PulseX V2.1 constants

---
+++ connectors/pulsex/constants.py
@@ -0,0 +1,12 @@
+""" Technical implementation for Hummingbot Gateway V2.1. """
+
+# PulseChain Network Configuration
+CHAIN_ID = 369
+NATIVE_TOKEN = "PLS"
+
+# PulseX V2 Main Router & Factory
+# Official addresses from PulseChain infrastructure
+PULSEX_ROUTER = "0x165C3410fC63255D508a3f81eEa218d6f95f4eE0"
+PULSEX_FACTORY = "0x29ba5887AF242858739679f2231E0099478f799C"
+
+CONNECTOR_NAME = "pulsex"
