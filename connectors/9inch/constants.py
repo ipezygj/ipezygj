29a5b3f05cb289a31093b8d44381ad56b5911312
ipezygj <ipezygj2@gmail.com>
2026-02-26 03:02:43 +0200

    feat: initialize 9inch connector with V2.1 router and factory constants

---
+++ connectors/9inch/constants.py
@@ -0,0 +1,8 @@
+""" Technical implementation for Hummingbot Gateway V2.1. """
+
+# 9inch DEX V2.1 Official Settings
+NINE_INCH_ROUTER = "0x98bf93ebf5c380C0e6Ae8e192A7e2AE08edAcc02"
+NINE_INCH_FACTORY = "0x32337f763266e51f49679198533682910795c344"
+
+# Network: PulseChain
+CHAIN_ID = 369
